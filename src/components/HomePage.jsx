2bba81398f2b94b366598441d8c77589f0306c00
CrazyScientist1901 <83540307+CrazyScientist1901@users.noreply.github.com>
2024-12-18 23:52:29 +0530

    Create HomePage.jsx

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+// HomePage.jsx
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div className="text-center p-6">
+      <h1 className="text-4xl font-bold">Welcome to GdgMovie</h1>
+      <p className="text-gray-400">Explore top-rated movies, categories, and more!</p>
+    </div>
+     <div>
+      {children}
+    </div>
+  );
+}
