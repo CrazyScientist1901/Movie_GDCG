6e05087dd0157cda26c4fccd2ee9c9aa24f0c949
CrazyScientist1901 <83540307+CrazyScientist1901@users.noreply.github.com>
2024-12-19 14:12:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ export default function App() {
         <Routes>
           {/* Home route */}
           <Route
-            path="/"
+            path="/HomePage"
             element={<HomePage addToCart={addToCart} />}
           />
 
